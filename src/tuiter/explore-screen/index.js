222c2ee53b46900a6f53d490cfe18645185c9e25
Anuja Prakash Kolse <anujaprakashkolse@Anujas-MBP.cable.rcn.com>
2023-05-30 18:56:19 -0400

    fixing netlify errors #2

--- src/tuiter/explore-screen/index.js
+++ src/tuiter/explore-screen/index.js
@@ -36,7 +36,7 @@ function ExploreScreen() {
        </li>
      </ul>
      <div className="position-relative mb-2">
-       <img src="/images/spacex1.png" className="w-100" alt="failed to load image"/>
+       <img src="/images/spacex1.png" className="w-100"/>
        <h1 className="position-absolute wd-nudge-up text-white wd-image-font">
          SpaceX Starship</h1>
      </div>
