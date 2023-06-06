5bf3e575b0870ce1b131a71125c13bb231a13208
Anuja Prakash Kolse <anujaprakashkolse@Anujas-MBP.cable.rcn.com>
2023-06-06 16:08:10 -0400

    minor indentation changes for tuit stats

--- src/tuiter/tuits/tuit-stats.js
+++ src/tuiter/tuits/tuit-stats.js
@@ -5,13 +5,13 @@ const TuitStats = ({ stats }) => {
         <div className="row">
             <ul className="nav nav-fill mt-2 text-secondary">
                 <li className="nav-item col col-3">
-                    <i className="bi bi-chat-left-dots"></i> {stats.replies}
+                    <i className="bi bi-chat-left-dots me-2"></i> {stats.replies}
                 </li>
                 <li className="nav-item col col-3">
-                    <i className="bi bi-repeat"></i> {stats.retuits}
+                    <i className="bi bi-repeat me-2"></i> {stats.retuits}
                 </li>
                 <li className="nav-item col col-3">
-                    <span style={{ marginRight: '5px' }}> {stats.liked ? <i style={{ color: 'red' }} className="bi bi-heart-fill"></i> : <i className="bi bi-heart"></i>}</span>{stats.likes}
+                    <span style={{ marginRight: '5px' }}> {stats.liked ? <i style={{ color: 'red' }} className="bi bi-heart-fill me-2"></i> : <i className="bi bi-heart"></i>}</span>{stats.likes}
                 </li>
                 <li className="nav-item col col-3">
                     <i className="bi bi-upload"></i>
