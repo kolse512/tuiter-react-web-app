d3aa639555f2a6c39118fdbe4d2c4b663daa170b
Anuja Prakash Kolse <anujaprakashkolse@Anujas-MBP.cable.rcn.com>
2023-06-13 13:35:32 -0400

    changed link for auth-services file(render.com api)

--- src/tuiter/services/auth-service.js
+++ src/tuiter/services/auth-service.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-const SERVER_API_URL = process.env.REACT_APP_SERVER_API_URL;
+// const SERVER_API_URL = process.env.REACT_APP_SERVER_API_URL;
 // const SERVER_API_URL = "http://localhost:4000/api"
+const SERVER_API_URL = "https://tuiter-node-server-app-lej1.onrender.com/api"
 const USERS_URL = `${SERVER_API_URL}/users`;
 
 
